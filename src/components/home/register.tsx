00cc14e936fd2d1afe43de6bc5b7ad2fb22315ad
LionButt93 <cstennis05@gmail.com>
2023-07-21 14:46:26 -0400

    began register page

--- src/components/home/register.tsx
+++ src/components/home/register.tsx
@@ -1,6 +1,40 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 const register: React.FC<any> = () => {
+
+
+  const [user,setUser] = useState({
+
+    password: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+
+});
+
+const handleChange = (e:any) => {
+  setUser({...user, [e.target.name]: e.target.value });
+}
+
+const registerUser = (e:any) => {
+  e.preventDefault();
+  axios
+    .post('http://', {...user})
+    .then((response) =>{
+      console.log('Response', response.data );
+    })
+    .catch((error) => {
+      console.log(error);
+
+    })
+}
+
+
+
+
+
+
   return (
     <div>register</div>
   )
