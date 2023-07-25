4f292fa7cae5d78cb07a43f9daa4b87671d3f3fa
LionButt93 <cstennis05@gmail.com>
2023-07-25 11:20:06 -0400

    working on register logic

--- src/components/home/Register.tsx
+++ src/components/home/Register.tsx
@@ -12,6 +12,7 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import axios from "axios";
 
 function RegistrationPageDemo() {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -24,6 +25,8 @@ function RegistrationPageDemo() {
       email: data.get("email"),
       password: data.get("password"),
     });
+
+
   };
 
   return (
